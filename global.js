165a0fd255393697a35d852a58a0f2c706bc158d
Mike Borgese <mikeborgese4@gmail.com>
2021-01-30 20:12:11 -0500

    Initial commit

---
+++ global.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    console.log('ff');
+    $( ".nav-item" ).hover(
+        function() {
+            $( this ).addClass('show');
+            $( this ).find('.nav-link').attr("aria-expanded", "true");
+            $( this ).find('.dropdown-menu').addClass('show');;
+
+        }, function() {
+            $( this ).removeClass('show');
+            $( this ).find('.nav-link').attr("aria-expanded", "false");
+            $( this ).find('.dropdown-menu').removeClass('show');;
+
+
+        }
+    );
+});
